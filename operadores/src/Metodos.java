2601ff84eaf78bef066439ca637ae9ee8493673c
Gustavo Schmidt <gustavo.diiegosch@gmail.com>
2023-07-13 13:22:22 -0300

    Iniciod de Métodos

---
+++ operadores/src/Metodos.java
@@ -0,0 +1,20 @@
+public class Metodos {
+    public static void main(String[] args){
+        
+    //Exemplo de nomemclatura de metodos    
+    /*somar(int n1, int n2){};
+    abrirConexao(){}
+    concluirProcessamento(){}
+    findById(int id){}
+    calcularImprimir(){} 
+    */
+
+    //Critério de definição de métodos
+    /*1º Qual a proposta do metodo?
+     * 2º Qual o tipo de retorno do metodo? int, double, void, boolean
+     * 3º Quais os parametros que serão necessarios?
+     * 4º Possui o risco de apresentar alguma  exceção?
+     * 5º  Qual a visibilidade do métodos? s
+     */
+}
+}
